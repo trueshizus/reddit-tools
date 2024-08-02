80de1054f409d37461e759573ca50a4dd72f5bb6
trueshizus <jesus.herrera.90@gmail.com>
2024-08-02 21:15:30 +0200

    Add revalidation

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,5 +1,6 @@
 import type { NextRequest } from "next/server";
 import client from "@/lib/mongodb";
+import { revalidatePath } from "next/cache";
 
 export async function GET(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
@@ -15,5 +16,6 @@ export async function GET(request: NextRequest) {
   const { insertedId } = await collection.insertOne(doc);
   const createdDoc = await collection.findOne({ _id: insertedId });
 
+  revalidatePath("/");
   return Response.json(createdDoc);
 }
